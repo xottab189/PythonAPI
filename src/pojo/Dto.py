af9ce53f7ebd942435ec22bbd4b1035959a26f6f
Nikolay <admin54@admkor.ru>
2026-08-02 12:09:07 +0300

    Парсим в dto

--- src/pojo/Dto.py
+++ src/pojo/Dto.py
@@ -3,6 +3,13 @@ from typing import Any, List
 from pydantic import BaseModel
 
 
+class DataResourceDto(BaseModel):
+    id: int
+    email: str
+    first_name: str
+    last_name: str
+    avatar: str
+
 
 class ResourceDto(BaseModel):
     page: int
@@ -11,12 +18,3 @@ class ResourceDto(BaseModel):
     total_pages: int
     data: List[DataResourceDto]
     support: Any = None
-
-
-
-class DataResourceDto(BaseModel):
-    id: int
-    email: str
-    first_name: str
-    last_name: str
-    avatar: str
